62d33bcb433c9efc5c0edb137dd7a81386bb2ee2
smitshah1711 <61113890+smitshah1711@users.noreply.github.com>
2024-04-03 11:01:48 -0400

    Update signin.go
    I am pushing signin.go file into repository.

--- handlers/signin.go
+++ handlers/signin.go
@@ -1,9 +1,44 @@
 package handlers
 
 import (
+	"encoding/json"
 	"net/http"
+
+	"golang.org/x/crypto/bcrypt"
+	"usermanagement.com/authentication"
+	"usermanagement.com/database"
 )
 
 func SignInHandler(w http.ResponseWriter, r *http.Request) {
+	var user database.User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 
+	// Retrieve user from the database
+	var storedUser database.User
+	if err := database.Db.Where("username = ?", user.Username).First(&storedUser).Error; err != nil {
+		http.Error(w, "User not found", http.StatusUnauthorized)
+		return
+	}
+
+	// Compare hashed password with the provided password
+	err = bcrypt.CompareHashAndPassword([]byte(storedUser.Password), []byte(user.Password))
+	if err != nil {
+		http.Error(w, "Invalid username or password", http.StatusUnauthorized)
+		return
+	}
+
+	// Generate JWT token
+	tokenString, err := authentication.GenerateJWT(user.Username)
+	if err != nil {
+		http.Error(w, "Failed to generate token", http.StatusInternalServerError)
+		return
+	}
+
+	// Return the JWT token in the response
+	json.NewEncoder(w).Encode(map[string]string{"token": tokenString})
 }
+
